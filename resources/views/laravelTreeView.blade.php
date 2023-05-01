0635d5cba119dc01d2cfd83c8838ca5879a6e4fc
Softlabltd.com <freelancernishad123@gmail.com>
2023-05-01 11:40:33 +0600

    update

--- resources/views/laravelTreeView.blade.php
+++ resources/views/laravelTreeView.blade.php
@@ -1,8 +1,8 @@
     <li>
 
       {{-- <span class="tf-nc" title="{{ $tree->username }}" onclick="viewUserDetials('{{ $tree->username }}','{{ $tree->name }}','{{ $tree->lastname }}')"> --}}
-      <span class="tf-nc" title="{{ $tree->username }}" onclick="updateTree('{{ $tree->id }}')">
-        <img  src="{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}" alt="">
+      <span class="tf-nc" title="{{ $tree->username }}" onclick="updateTree('{{ $tree->id }}')" style="width: 50px; height: 50px; background-image: url('{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}'); display:block;    background-size: cover;">
+        {{-- <img  src="{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}" alt=""> --}}
         {{-- {{ $tree->username }} --}}
     </span>
       @if(count($tree->children) > 0)
