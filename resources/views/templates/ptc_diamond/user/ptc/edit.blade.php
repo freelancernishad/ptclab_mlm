ffc6daf76f1d88f8ca2f9290908d3d636d0356a2
Softlabltd.com <freelancernishad123@gmail.com>
2023-05-01 11:05:48 +0600

    update

--- resources/views/templates/ptc_diamond/user/ptc/edit.blade.php
+++ resources/views/templates/ptc_diamond/user/ptc/edit.blade.php
@@ -7,6 +7,7 @@
 <div class="card-body">
     <form role="form" method="POST" action="{{ route("user.ptc.update",$ptc->id) }}" enctype="multipart/form-data">
         @csrf
+        <input type="hidden" name="IfrOr" value="{{ $ptc->IfrOr }}">
         <div class="row">
             <div class="form-group col-md-6">
                 <label class="form-label">@lang('Title of the Ad')</label>
@@ -28,6 +29,10 @@
                     @php echo $ptc->typeBadge @endphp
                 </div>
             </div>
+
+            @if($ptc->IfrOr=='iframe')
+
+
             @if($ptc->ads_type == 1)
 
             <div class="form-group col-md-8">
@@ -58,6 +63,42 @@
                     <input type="text" name="youtube" class="form-control form--control" value="{{ $ptc->ads_body }}">
                 </div>
             @endif
+
+
+            @else
+
+            <div class="form-group col-md-12">
+                <label>@lang('Description') <span class="text-danger">*</span></label>
+                <textarea class="tinymce-editor" name="ads_body">{{ $ptc->ads_body }}</textarea>
+
+            </div>
+
+            @php
+               $filesSupports = json_decode($ptc->filesSupports);
+            @endphp
+
+            <div class="form-group col-md-12">
+                <label class="form-label">@lang('Prove Files')</label>
+                <select class="select2-multi-select form-control" name="filesSupports[]" multiple>
+                    <option @if(is_array($filesSupports))  @if(in_array('jpg', $filesSupports)) selected @endif @endif value="jpg">JPG</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('jpeg', $filesSupports)) selected @endif @endif value="jpeg">JPEG</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('png', $filesSupports)) selected @endif @endif value="png">PNG</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('mp4', $filesSupports)) selected @endif @endif value="mp4">MP4</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('pdf', $filesSupports)) selected @endif @endif value="pdf">PDF</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('doc', $filesSupports)) selected @endif @endif value="doc">DOC</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('docx', $filesSupports)) selected @endif @endif value="docx">DOCX</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('txt', $filesSupports)) selected @endif @endif value="txt">TXT</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('xlx', $filesSupports)) selected @endif @endif value="xlx">XLX</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('xlsx', $filesSupports)) selected @endif @endif value="xlsx">XLSX</option>
+                    <option @if(is_array($filesSupports))  @if(in_array('csv', $filesSupports)) selected @endif @endif value="csv">CSV</option>
+                </select>
+
+            </div>
+
+
+            @endif
+
+
         </div>
         <div class="form-group col-md-12">
             <button type="submit" class="btn btn--base btn--lg w-100">@lang('Submit')</button>
