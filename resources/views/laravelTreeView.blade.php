fc5fb1a841a5bf5decda9f91f90267a61c66e3ed
Softlabltd.com <freelancernishad123@gmail.com>
2023-05-01 11:42:27 +0600

    up

--- resources/views/laravelTreeView.blade.php
+++ resources/views/laravelTreeView.blade.php
@@ -1,7 +1,7 @@
     <li>
 
       {{-- <span class="tf-nc" title="{{ $tree->username }}" onclick="viewUserDetials('{{ $tree->username }}','{{ $tree->name }}','{{ $tree->lastname }}')"> --}}
-      <span class="tf-nc" title="{{ $tree->username }}" onclick="updateTree('{{ $tree->id }}')" style="width: 50px; height: 50px; background-image: url('{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}'); display:block;    background-size: cover;">
+      <span class="tf-nc" title="{{ $tree->username }}" onclick="updateTree('{{ $tree->id }}')" style="width: 50px; height: 50px; background-image: url('{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}'); display:block;    background-size: contain;background-position: center;background-repeat: no-repeat;">
         {{-- <img  src="{{ getImage(getFilePath('userProfile').'/'.$tree->image) }}" alt=""> --}}
         {{-- {{ $tree->username }} --}}
     </span>
